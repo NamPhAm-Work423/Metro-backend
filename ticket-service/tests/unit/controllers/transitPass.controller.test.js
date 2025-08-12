a4dc1907ac7bd881564710f8ff757bf955fb4aad
Nam Phạm <namphamwork423@gmail.com>
2025-08-12 19:23:29 +0700

    fix: change test error

--- ticket-service/tests/unit/controllers/transitPass.controller.test.js
+++ ticket-service/tests/unit/controllers/transitPass.controller.test.js
@@ -1,13 +1,21 @@
 const request = require('supertest');
 const express = require('express');
 
+// Mock authorization middleware to bypass authentication
+jest.mock('../../../src/middlewares/authorization', () => ({
+  authorizeRoles: (...roles) => [
+    (req, res, next) => {
+      req.user = { id: 'test-user', roles: roles };
+      next();
+    }
+  ]
+}));
+
 const router = require('../../../src/routes/transitPass.route');
 
 describe('TransitPass Controller', () => {
   const app = express();
   app.use(express.json());
-  // inject dummy auth middleware bypass
-  app.use((req, res, next) => { req.user = { roles: ['admin'] }; next(); });
   app.use('/api/v1/ticket/transitPasses', router);
 
   it('should validate create payload', async () => {
